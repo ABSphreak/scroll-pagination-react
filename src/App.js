ecf61807cd0d52423e582012cbae26e1f87649b3
ABSphreak <absphreak@outlook.com>
2022-01-16 18:00:59 +0530

    🔧 Fix styling and add logic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useRef, useCallback } from 'react';
+import useBookSearch from './useBookSearch';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [query, setQuery] = useState('');
+	const [pageNumber, setPageNumber] = useState(1);
+
+	const { books, hasMore, loading, error } = useBookSearch(query, pageNumber);
+
+	const observer = useRef();
+	const lastBookElementRef = useCallback(
+		node => {
+			if (loading) return;
+			if (observer.current) observer.current.disconnect();
+			observer.current = new IntersectionObserver(entries => {
+				if (entries[0].isIntersecting && hasMore) {
+					setPageNumber(prevPageNumber => prevPageNumber + 1);
+				}
+			});
+			if (node) observer.current.observe(node);
+		},
+		[loading, hasMore]
+	);
+
+	const handleSearch = e => {
+		setQuery(e.target.value);
+		setPageNumber(1);
+	};
+
+	return (
+		<div className="App">
+			<div className="w-full flex items-center justify-center fixed top-0">
+				<input
+					type="text"
+					value={query}
+					placeholder="Search"
+					onChange={handleSearch}
+					className="w-full focus:outline-none shadow-sky-200 focus:shadow-blue-300 py-2 shadow-lg font-thin placeholder:text-sky-300 text-center focus:border-sky-500 text-sky-600 border-b text-5xl border-sky-300"
+				/>
+			</div>
+			<div className="text-center text-lg tracking-tight mt-20">
+				{books.map((book, index) => {
+					if (books.length === index + 1) {
+						return (
+							<div key={book.key} ref={lastBookElementRef}>
+								{book.title}
+							</div>
+						);
+					}
+					return <div key={book.key}>{book.title}</div>;
+				})}
+			</div>
+			{loading && <div className="text-pink-500 text-center text-xl font-semibold">Loading more...</div>}
+			{error && <div>Error!</div>}
+		</div>
+	);
 }
 
 export default App;
